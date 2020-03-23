a5f0415b730610e921686a6fbeca1512fa0619b8
Lisa Flinn <2386278F@student.gla.ac.uk>
2020-03-23 14:37:36 +0000

    feat: added template folder

--- manageflow/urls.py
+++ manageflow/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from manageflow.accounts import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
 ]
