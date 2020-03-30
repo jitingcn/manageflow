5d25e46dff7186ce166c14375f9e995526264d7e
jitingcn <jiting@jtcat.com>
2020-03-31 02:59:02 +0800

    fix(boards): add missing urls.py

---
+++ manageflow/boards/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import include, path
+from allauth.account.views import LoginView, SignupView, LogoutView
+from manageflow.boards import views
+
+urlpatterns = [
+    path("", views.index, name="mf-index"),
+    path("docs/", views.serve_doc, name="mf-docs"),
+    path("docs/<slug:doc>/", views.serve_doc, name="mf-serve-doc"),
+]
