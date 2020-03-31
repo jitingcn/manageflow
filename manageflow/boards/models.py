7395edc123385d7952a391440f0274a2b8134b02
Lisa Flinn <2386278F@student.gla.ac.uk>
2020-03-31 16:43:01 +0100

    chore: board now can be created in Admin

--- manageflow/boards/models.py
+++ manageflow/boards/models.py
@@ -6,7 +6,7 @@ from django.template.defaultfilters import slugify
 
 
 class Board(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	board_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	name = models.CharField(max_length=200)
 	description = models.TextField()
 
