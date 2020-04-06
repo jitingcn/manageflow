9e3d2dd7535a58eadc548cd363dfb86c66d0e43d
jitingcn <jiting@jtcat.com>
2020-04-07 06:35:32 +0800

    feat: complete board detail

--- manageflow/boards/views.py
+++ manageflow/boards/views.py
@@ -65,6 +65,7 @@ def about(request):
 
     return render(request, "about.html", ctx)
 
+
 @login_required
 def create_board(request, username):
     form = CreateNewBoard()
@@ -78,21 +79,24 @@ def create_board(request, username):
                 temp.admin = request.user
                 temp.save()
                 return redirect(f"/{request.user.get_username()}/boards")
-        return HttpResponseForbidden
+        return HttpResponseForbidden("")
 
     return render(request, 'boards/create_board.html', {'form': form})
 
 
 @login_required
 def dashboard(request, username):
-    UsersBoards= Board.objects.filter(admin=request.user)
-    context = {"board":UsersBoards}
-    return render(request, 'boards/boards.html', context)
+    if username == request.user.get_username():
+        UsersBoards = Board.objects.filter(admin=request.user)
+        context = {"board": UsersBoards}
+        return render(request, 'boards/boards.html', context)
+    else:
+        return HttpResponseForbidden("")
 
 
-def create_task(request,username,board_id):
+def create_task(request, username, board_id):
     form = CreateNewTask()
-    board= get_object_or_404(Board, id=board_id)
+    board = get_object_or_404(Board, id=board_id)
 
     if request.method == "POST":
         if username == request.user.get_username():
@@ -108,9 +112,25 @@ def create_task(request,username,board_id):
     return render(request, 'boards/task.html', {'form': form, 'board': board})
 
 
-
 def board_post_detail(request, board_id):
     obj = get_object_or_404(Board, id=board_id)
-    taskobj= Task.objects.filter(board=obj)
-    context = {"object": obj, "tasks": taskobj}
-    return render(request, 'boards/board_post_detail.html', context)
+    task_obj = Task.objects.filter(board=obj)
+
+    if request.method == "POST":
+        task_id = request.POST.get("action")
+        delete = request.POST.get("delete")
+        task_list = [str(task.id) for task in task_obj]
+        if task_id in task_list:
+            if delete == "yes":
+                task = Task.objects.get(id=task_id)
+                task.delete()
+            else:
+                task = Task.objects.get(id=task_id)
+                task.complete = True
+                task.save()
+            task_obj = Task.objects.filter(board=obj)
+            context = {"object": obj, "tasks": task_obj}
+            return render(request, 'boards/board_detail.html', context)
+
+    context = {"object": obj, "tasks": task_obj}
+    return render(request, 'boards/board_detail.html', context)
